0faad21f796c9708755e3e5d031621355666e6d8
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-07-08 14:58:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import mysql.connector
 import streamlit as st
 
-host = st.secret["HOST"]
-port = st.secret["PORT"]
-user = st.secret["USER"]
-password = st.secret["PASSWORD"]
-db = st.secret["DB"]
+host = st.secrets["HOST"]
+port = st.secrets["PORT"]
+user = st.secrets["USER"]
+password = st.secrets["PASSWORD"]
+db = st.secrets["DB"]
 conn = mysql.connector.connect(host = host,
                        port =port,
                        user = user,
